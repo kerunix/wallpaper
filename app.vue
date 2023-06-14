2abd07cb68e169dd9129479391bd1a6775a137b0
kerunix <pierre@kissmy.co>
2023-06-14 09:53:36 +0200

    chore(setup): testing js

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+const foo = ref('')
+
+onMounted(() => setTimeout(() => {
+  foo.value = 'AH QUE COUCOU'
+}, 10000))
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+      <div class="h-screen w-screen bg-background text-white flex items-center justify-center">
+        <h1>Hello world</h1>
+        <p>{{ foo }}</p>
   </div>
 </template>
