86f9c352a26764e0399cc19ba46a207446f0608c
kerunix <pierre@kissmy.co>
2023-06-14 10:22:05 +0200

    feat(clock): added basic clock

--- app.vue
+++ app.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
-const foo = ref('')
+const date = computed(() => new Intl.DateTimeFormat('fr-FR').format(new Date()))
+const time = ref('')
 
-onMounted(() => setTimeout(() => {
-  foo.value = 'AH QUE COUCOU'
-}, 10000))
+onMounted(() => setInterval(() => {
+  const hour = new Date().getHours()
+  const minutes = new Date().getMinutes()
+  const seconds = new Date().getSeconds()
+
+  time.value = `${hour}:${minutes}:${seconds}`
+}, 1000))
 </script>
 
 <template>
-  <div class="h-screen w-screen bg-background text-white flex items-center justify-center">
-    <h1>Hello world</h1>
-    <p>{{ foo }}</p>
+  <div class="h-screen w-screen bg-background text-white grid grid-cols-8 gap-12 p-20">
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60 flex flex-col space-y-11">
+      <span class="text-green text-7xl">{{ date }}</span>
+      <span class="text-pink text-6xl">{{ time }}</span>
+    </div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
+    <div class="col-span-1 rounded-sm h-60"></div>
   </div>
 </template>
